e26e73262f030730ff9a2e480d91b755b5e01643
Julio Chura <jchuraaca@unsa.edu.pe>
2025-11-25 20:57:24 -0500

    FIFO implementacion

--- simulador/src/main/java/memory/algoritmos/FIFO.java
+++ simulador/src/main/java/memory/algoritmos/FIFO.java
@@ -1,5 +1,42 @@
 package memory.algoritmos;
+import java.util.LinkedList;
+import java.util.Queue;
+import memory.Frame;
+import memory.PageTable;
+import java.util.List;
+import java.util.Map;
 
-public class FIFO {
-    
-}
+
+public class FIFO implements ReplacementAlgorithm {
+    private Queue<Integer> frameQueue; // cola de frames en orden de carga
+
+    public FIFO() {
+        this.frameQueue = new LinkedList<>();
+    }
+
+    @Override
+    public Integer chooseVictimFrame(List<Frame> phsycalMemory, Map<String, PageTable> processPageTables) {
+        // Elige el frame que lleva más tiempo en memoria (el primero en la cola)
+        return frameQueue.peek(); // solo mira el primero sin removerlo
+    }
+
+    @Override
+    public void onPageLoaded(String processId, int pageNumber, int frameId) {
+        frameQueue.add(frameId);
+    }
+
+    @Override
+    public void onPageAcces(String processId, int pageNumber) {
+        // No se necesita hacer nada para FIFO cuando se accede a una página
+    }
+
+    @Override
+    public void onPageUnloaded(String processId, int pageNumber) {
+        // No se necesita hacer nada para FIFO cuando se descarga una página
+    }
+
+    @Override
+    public String getName() {
+        return "FIFO";
+    }
+}   
